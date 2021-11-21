85c6ff60fa54d3bb1b6d311e1624925950bf7525
glenluersman <glen.luersman@gmail.com>
2021-11-20 20:13:24 -0500

    Setup Functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
+// View high scores link at top of page
+// Create 5 multiple choice questions to be answered
+// Display correct or incorrect upon answer
+// Next question upon correct answer 
+// 10 seconds off clock for inccorect answer
+// Display high score after all questions answered
 var timerEL = document.querySelector("#timer");
 var startEl = document.querySelector("#start");
+var questionEl = document.createElement("div");
+var listEl = document.createElement("ol");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+var li5 = document.createElement("li");
 
+// Create a countdown in the length of 60 seconds
 var countDown = function() {
     var timeLeft = 60;
 
@@ -18,3 +32,9 @@ var countDown = function() {
 startEl.addEventListener('click', function() {
     countDown()
 });
+
+var questionEl = function() {
+    listEl.setAttribute("style", "padding 10px");
+    listEl.appendChild(li1);
+    li1.setAttribute()
+};
