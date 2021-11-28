3ad5ae1ea1d70f9d7b045f26f15ffecc6a78e5b2
glenluersman <glen.luersman@gmail.com>
2021-11-27 19:48:40 -0500

    display quiz questions setup HTML for scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,126 @@
 // View high scores link at top of page
-// Display first question
-// Display correct or incorrect upon answer
-// Next question upon correct answer 
-// 10 seconds off clock for inccorect answer
-// Display high score after end of quiz
-var timerEL = document.querySelector("#timer");
-var startButton = document.querySelector("#start");
-var quizContainer = document.getElementById("quiz");
-var resultsContainer = document.getElementById("results");
-var submitButton = document.getElementById("submit");
+
+
+ 
+
+
+var timerEL = document.getElementById("time-counter");
+var alert = document.getElementById("alert");
+var info = document.getElementById("info");
+var titleItem = document.getElementById("title-item");
+var quizQuestions = document.getElementById("quiz-questions");
+var btnStart = document.getElementById("btn-start");
+var timer = document.getElementById("timer");
+var questionAnswers = document.getElementById("question-answers");
+var currentIndex = 0;
+var timeLeft = 75;
+var nextQuestion
+//var score = 0;
+//var allScores = [];
+//var storedScores = JSON.parse(localStorage.getItem("userData"));
+
 // Create array of questions
 var myQuestions = [
     {
         question: "What are variables used for in Javascript Programs?",
-        answers: {
-            a: "Storing numbers, dates, or other values",
-            b: "Varying randomly",
-            c: "Causing high-school algebra flashbacks",
-            d: "None of the above"
-        },
-        correctAnswer: "a"
+        answers: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks", "None of the above"],
+        correctAnswer: "Storing numbers, dates, or other values"
     },
     {
         question: "_______ tag is an extension to HTML that caan enclose any number of Javascript statements.",
-        answers: {
-            a: "<script>",
-            b: "<body>",
-            c: "<head>",
-            d: "<title>"
-        },
-        correctAnswer: "a"
+        answers: ["<script>", "<body>", "<head>", "<title>"],
+        correctAnswer: "<script>"
     },
     {
         question: "Using ______ statement is how you test for a specific condition.",
-        answers: {
-            a: "Select",
-            b: "If",
-            c: "Switch",
-            d: "For"
-        },
-        correctAnswer: "b"
+        answers: ["Select", "If", "Switch", "For"],
+        correctAnswer: "If"
     },
     {
         question: "In Javascript, Window.prompt() method return true or false value?",
-        answers: {
-            a: "False",
-            b: "True"
-        },
-        correctAnswer: "a"
+        answers: ["False", "True"],
+        correctAnswer: "False"
     },
     {
         question: "What is the correct Javascript syntax to write 'Hello World'?",
-        answers: {
-            a: "System.out.printIn('Hello World')",
-            b: "printIn ('Hello World')",
-            c: "document.write('Hello World')",
-            d: "response.write('Hello World')"
-        },
-        correctAnswer: "c"
+        answers: ["System.out.printIn('Hello World')", "printIn ('Hello World')", "document.write('Hello World')", "response.write('Hello World')"],
+        correctAnswer: "document.write('Hello World')"
     }
 ];
 
-// Create a countdown in the length of 60 seconds
-var countDown = function() {
-    var timeLeft = 60;
+btnStart.addEventListener("click", startQuiz);
 
+function startQuiz() {
+    info.classList.add("d-none")
+    btnStart.classList.add("d-none")
+    timer.classList.remove("d-none")
+    quizQuestions.classList.remove("d-none")
+    nextQuestion = myQuestions[currentIndex]
+    console.log(nextQuestion.question);
+    
+    displayQuestion(nextQuestion)
+    
+    countDown()
+};
+
+function countDown() {
+    
     var timeInterval = setInterval(function() {
-        if(timeLeft > 0) {
-            timerEL.innerText = timeLeft + " Seconds Remaining";
-            timeLeft--;
-        } else {
-            timerEL.innerText = "0 Seconds Remaining";
-        }
+        timer.innerText = timeLeft
+        timeLeft--;
     },1000);
 };
 
+// Display first question
+function displayQuestion(myQuestions) {
+    titleItem.innerText = myQuestions.question
+    Array.from(myQuestions.answers).forEach(element => {
+      var button = document.createElement("button")
+    button.className = "answerbtn btn-primary btn-block text-left"
+    button.innerText = element
+    questionAnswers.appendChild(button)
+    button.addEventListener("click", displaynextQuestion)
+    });
+};
 
-var buildQuiz = function() {}
+// Next question upon answer
+function displaynextQuestion(event) {
+    currentIndex++
+    if(currentIndex < myQuestions.length){
+        correction(event.target.innerText == nextQuestion.correctAnswer)
+        questionAnswers.innerHTML=""
+        if(currentIndex < myQuestions.length){    
+            nextQuestion= myQuestions[currentIndex]
+            displayQuestion(nextQuestion)  
+        }else {
+            currentIndex = 0
+            displayQuestion(nextQuestion)  
+        }
+        
+    }else{
+        endgame()
+        
+        
+    }
+};
 
-var showResults = function() {}
-
-startButton.addEventListener('click', function() {
-    countDown()
-    buildQuiz()
-});
+// Display correct or incorrect upon answer
+function correction(response) {
+    
+    if(response){
+        alert.innerText= "Correct"
+    }else {
+        alert.innerText="Incorrect"
+        // 10 seconds off clock for inccorect answer
+        timeLeft = timeLeft -10
+        timer.innerHTML = timeLeft
+    }
+    setTimeout(function(){
+        alert.innerText=""
+    }, 1000);
+    
+};
+function endgame() {
+    timer.classList.add("d-none")
+    quizQuestions.classList.add("d-none")
+};
